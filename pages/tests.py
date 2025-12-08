e94bba3847d63c554922390c16bf1ef69b8affd9
Samyog Maharjan <msamyog37@gmail.com>
2025-12-07 21:00:41 -0600

    Added test code

--- pages/tests.py
+++ pages/tests.py
@@ -1,33 +1,44 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from .models import Post
 
 # Create your tests here.
 
-class PostModelTest(TestCase):
+
+class BlogTests(TestCase):
     def setUp(self):
-        Post.objects.create(text='just a test')
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='secert'
+        )
 
-    def test_text_content(self):
-        post=Post.objects.get(id=1)
-        expected_object_name=f'{post.text}'
-        self.assertEqual(expected_object_name,'just a test')
-
-class HomePageViewTest(TestCase):
-    def setUp(self):
-        Post.objects.create(text='this is another test')
-
+        self.post = Post.objects.create(
+            title='A good title',
+            body='Nice Body content',
+            author=self.user,
+        )
     
-    def test_view_url_exists_at_proper_location(self):
-        response=self.client.get('/')
-        self.assertEqual(response.status_code,200)
+    def test_string_representation(self):
+        post = Post(title='A sample title')
+        self.assertEqual(str(post), post.title)
 
-        
-    def test_view_url_by_name(self):
-        response=self.client.get(reverse('home'))
-        self.assertEqual(response.status_code,200)
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A good title')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'Nice Body content')
 
-    def test_view_uses_correct_template(self):
-        resp=self.client.get(reverse('home'))
-        self.assertEqual(resp.status_code,200)
-        self.assertTemplateUsed(resp,'home.html')
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nice Body content')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('/post/1')
+        no_response = self.client.get('/post/10000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'A good title')
+        self.assertTemplateUsed(response, 'post_detail.html')
