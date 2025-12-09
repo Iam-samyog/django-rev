18adf10548f0e2b5721e29e1fc490a55d148516b
Samyog Maharjan <msamyog37@gmail.com>
2025-12-08 20:17:45 -0600

    Completed the rev of CRUD operations

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView,TemplateView,DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 class HomePageView(ListView):
     model=Post
@@ -20,3 +21,12 @@ class BlogCreateView(CreateView):
     template_name='post_new.html'
     fields='__all__'
 
+class BlogUpdateView(UpdateView):
+    model=Post
+    template_name='post_edit.html'
+    fields=['title','body']
+
+class BlogDeleteView(DeleteView):
+    model=Post
+    template_name='post_delete.html'
+    success_url=reverse_lazy('home')
